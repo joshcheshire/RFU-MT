ab4ea70d1bd14d5ce5846a2406a3efdd9b06e47f
Student <student@RU-017.local>
2015-09-25 16:25:45 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+//start by making constructors for each city for the demo
+var test = function(){
+	console.log('test')
+}
+test()
+
+var City = function(name, zip-codes, performing){
+
+	this.name       = name
+	this.zip-codes = zip-codes
+	this.performing = performing
+
+}
