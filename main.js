5d96ed6473a59e61031714aeb48ce9c9752ee19b
Student <student@RU-017.local>
2015-09-29 14:31:49 -0600

    some updates

--- main.js
+++ main.js
@@ -1,13 +1,70 @@
-//start by making constructors for each city for the demo
-var test = function(){
-	console.log('test')
-}
-test()
+// var test = function(){
+// 	console.log('test')
+// }
+// test()
 
-var City = function(name, zip-codes, performing){
 
-	this.name       = name
-	this.zip-codes = zip-codes
-	this.performing = performing
 
-}
+var master = angular.module('master', [])
+
+
+
+
+master.controller('mainController', function($scope){
+	
+$scope.showTheData = false
+
+	$scope.findBands = function(){
+		
+		$scope.showData = function(event){
+
+
+$scope.showTheData = !$scope.showTheData
+console.log('click works')
+
+		}
+
+
+$scope.bandList = bandList
+
+	}
+
+})
+
+
+var bandList = [{
+
+		headliner:'And So I watch You From Afar',
+		// headlinerSound: href = 'https://m.soundcloud.com/beartrappr/floating-somewhere-high-and-above',
+		supporters:[{
+		name:'Mylet'
+		}, 
+		
+		{name:'Blis.' 
+
+		},
+		{name:'Cult of the Lost Cause'
+		
+		}],
+		
+		city:'Denver' ,
+		venue: 'Marquis Theater',
+		date: 'October 2, 2015',
+},
+{
+		headliner: 'Ben Howard',
+		city: 'Denver' ,
+		venue: 'Red Rocks',
+		date: 'October 2, 2015',
+
+},
+{		headliner: 'Zappa Plays Zappa',
+		city: 'Boulder' ,
+		venue: 'Boulder Theater',
+		date: ' October 2, 2015'
+
+
+}]
+
+console.log(bandList)
+
